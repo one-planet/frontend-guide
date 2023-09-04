06951ce3e063ebf86237d465c9f8545b48773089
one-planet <your-email@example.com>
2023-09-04 09:38:29 -0500

    cleaned comments

--- storefront-design/src/components/Header/Header.tsx
+++ storefront-design/src/components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import {FunctionComponent} from 'react'
 import logo from '/logo.svg'
-// import {CartWidget} from '../CartWidget'
 import classes from './header.module.scss'
 import shoppingCart from '../../assets/shopping-cart.svg'
 
@@ -11,9 +10,9 @@ export const Header: FunctionComponent = () => {
             <div>
                 <img src={logo} alt="Shopping Cart Application" className={classes.logo}/>
             </div>
-            <h3 className={classes.title}>Products</h3>            
-            <button className={classes.container}>
-                <img src={shoppingCart} alt="Go To Cart" className={classes.shoppingCart}/>
+            <h3>Products</h3>            
+            <button>
+                <img src={shoppingCart} alt="Go To Cart" className={classes.logo}/>
             </button>
 
         </header>
--- storefront-design/src/components/Products/Products.tsx
+++ storefront-design/src/components/Products/Products.tsx
@@ -3,11 +3,11 @@ import classes from './products.module.scss'
 import thumbnail from "../../assets/thumbnail.jpg"
 
 export const Products: FunctionComponent = () => {
-  const id = [1,2,3,4,5,6,7,8,9];
+  const products = [1,2,3,4,5,6,7,8,9];
   const title = "iPhone 9";
   const price = 109;
-  const productList = id.map( () => (
-    <div className={classes.product} onClick={() => console.log("howdy")}>
+  const productList = products.map( (id) => (
+    <div key={id} className={classes.product} onClick={() => console.log("howdy")}>
       <img src={thumbnail} alt={title}></img>
       <h3>{title}</h3>
       <p>Price: ${price}</p>
@@ -17,7 +17,6 @@ export const Products: FunctionComponent = () => {
 
   return (
     <section className={classes.productPage}>
-      {/* <h1>Products</h1> */}
       <div className={classes.container}>
         {productList}
       </div>
